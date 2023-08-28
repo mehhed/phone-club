a253a1a908356582317fe0f6fe5eaa9e52a45c8a
mehedi hasan <mehedih4700@gmail.com>
2023-08-29 00:18:46 +0600

    bal

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ const loadPhone = async (sarchdata) => {
     loadSingleData(PhoneData)
 }
 
-function loadSingleData(allData) {
-    let allDatas = allData;
-    allDatas2 = allDatas.slice(0, 10);
+function  loadSingleData(data){
     let allPhoneContainer = document.getElementById('Phone-display-container');
+    let  allDatas2 = data
     allDatas2.forEach(data => {
         let productDiv = document.createElement('div');
         productDiv.classList.add('card', 'bg-base-100', 'shadow-xl')
@@ -20,19 +19,20 @@ function loadSingleData(allData) {
                 <div class="card-body items-center text-center">
                   <p class="text-2xl font-semibold">${data.brand}</p>
                   <h2 class="card-title">${data.phone_name}</h2>
-                    <button class="btn bg-blue-500 text-white">Show Details</button>
+                  <label for="my_modal_6" class="btn">open modal</label>
                   </div>
                 </div>
         `;
         allPhoneContainer.appendChild(productDiv)
-        console.log(data);
+        // console.log(data);
     });
 }
+
+
 loadPhone("samsung");
 function sarch(){
     let allPhoneContainer = document.getElementById('Phone-display-container');
     allPhoneContainer.innerText = '';
     let sarchInput = document.getElementById('sarch-input').value;
-    console.log(sarchInput);
     loadPhone(sarchInput)
 }
